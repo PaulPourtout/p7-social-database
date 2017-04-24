355aef0e667a8a72686d8039d4aaa02238484c73
PaulPourtout <paulpourtout@hotmail.com>
2017-04-24 16:00:31 +0200

    added procfile

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express'),
-	app = express(),
-	port = process.env.PORT || 8080;
+  app = express(),
+  Sequelize = require('sequelize'),
+  port = process.env.PORT || 8080;
+
+var sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+// Or you can simply use a connection uri
+var sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 
 app.use('/', (req, res) => {
-	res.send('Cest la route !!!!');
-})
-
-.listen(port, (req, res) => {
-	console.log('Server is Ok. Listening port ', port);
+  res.send('Cest la route !!!!');
+}).listen(port, (req, res) => {
+  console.log('Server is Ok. Listening port ', port);
 });
